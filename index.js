1302d30f72f62900102f76bfad9aa961d0dfcc79
“Ezgi <okurezgi@windowslive.com>
2022-05-06 18:41:06 +0300

    Done with phase-1-array-find-method-lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(array){
+    let iterator = (object) => {
+       return (object.result === "W")
+    }
+   if (array.find(iterator)!==undefined){
+     return array.find(iterator).year
+   } return array.find(iterator)
+}
